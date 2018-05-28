7a0a4089d62e15779e02b7c0800ebc58d8dcf767
AnNguyen97 <an.nguyenit97@gmail.com>
2018-05-29 00:17:07 +0300

    Complete exercise 5

--- rna-transcription/Sources/RnaTranscription.swift
+++ rna-transcription/Sources/RnaTranscription.swift
@@ -1,3 +1,36 @@
 //Solution goes in Sources
 
 //Test using SWITCH
+class Nucleotide {
+    var dnaString: String
+    
+    
+    enum TranscriptionError: Error {
+        case invalidNucleotide
+
+    }
+    
+    init(_ dna: String) {
+        self.dnaString = dna
+    }
+    
+    func complementOfDNA() throws -> String {
+        var rnaString = ""
+        for character in dnaString {
+            switch character {
+            case "G":
+                rnaString += "C"
+            case "C":
+                rnaString += "G"
+            case "T":
+                rnaString += "A"
+            case "A":
+                rnaString += "U"
+            default:
+                throw TranscriptionError.invalidNucleotide
+            }
+        }
+        return rnaString
+    }
+}
+
