8e1c040a227dbf5c3eb0b4ddaa7dc4772c3116a7
AnNguyen97 <an.nguyenit97@gmail.com>
2018-05-29 23:32:16 +0300

    Solution using map, filter and reduce for collection

--- sum-of-multiples/Sources/SumOfMultiples.swift
+++ sum-of-multiples/Sources/SumOfMultiples.swift
@@ -1,5 +1,6 @@
 //Solution goes in Sources
-class SumOfMultiples  {
+
+/* class SumOfMultiples  {
     class func toLimit(_ limit: Int, inMultiples: [Int]) -> Int {
         var sum = 0
         var valueArray = inMultiples
@@ -26,7 +27,41 @@ class SumOfMultiples  {
         
         return sum
     }
+} */
+
+class SumOfMultiples  {
+    static func findMultiple(_ limit: Int, for multiple: Int) -> [Int] {
+        if multiple >= limit {
+            return [Int]()
+        }
+        
+        let number = multiple..<limit
+        return number.filter {
+            if multiple == 0 {
+                return false
+            }
+            
+            if $0 == 0 {
+                return false
+            }
+            
+            if $0 > multiple {
+                return $0 % multiple == 0
+            } else {
+                return multiple % $0 == 0
+            }
+        }
+    }
+    
+    static func toLimit(_ limit: Int, inMultiples: [Int]) -> Int {
+        let arrayOfMultiples = inMultiples.map {  findMultiple(limit, for: $0) }
+        let multiples = Array(Set(arrayOfMultiples.joined()))
+        let sum = multiples.reduce(0, +)
+        return sum
+    }
 }
+
+
 
 
 
